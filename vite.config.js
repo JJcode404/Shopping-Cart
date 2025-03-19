6eb313744ada91eb61adc04f6d3f995f6a84dbc1
JJcode404 <khamjapher1@gmail.com>
2025-03-19 18:59:01 +0300

    feat: deploy

--- vite.config.js
+++ vite.config.js
@@ -4,16 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        entryFileNames: "index.js", // Ensures a stable output filename
-      },
-    },
-  },
-  resolve: {
-    alias: {
-      fs: "browserify-fs", // Example for fixing `fs` module
-    },
-  },
 });
